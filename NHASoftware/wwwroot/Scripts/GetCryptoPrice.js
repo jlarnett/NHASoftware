5622f11d81979cad9a62915c0b392e434ae6bffc
Johnny Arnett <im10g@hotmail.com>
2021-12-09 17:47:39 -0500

    Added various improvements. Tried to get CORS for outside crypto call.

---
+++ NHASoftware/wwwroot/Scripts/GetCryptoPrice.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function() {
+    $("#Submit").click(function() {
+        $.ajax({
+            url: 'https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT',
+            method: 'GET',
+            contentType: "application/json; charset=utf-8",
+            datatype: 'json',
+            success: function(data) {
+                if (data.success) {
+                    $("#CryptoLabel").text(data.symbol);
+                    $("#CryptoPrice").text(data.price);
+                }
+            }
+        });
+    });
+});
