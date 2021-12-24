44cdd756a0d5a6f37fafefc91632e8ebbd76e93b
Johnny Arnett <im10g@hotmail.com>
2021-12-23 19:55:56 -0500

    More large changes. Added Mail Kit nuget package and setup the recurring hangfire jobs to send email to the associated user. Something is off with the timing of the recurring jobs being created. Need to investigate

---
+++ NHASoftware/Models/EmailData.cs
@@ -0,0 +1,11 @@
+﻿namespace NHASoftware.Models
+{
+    public class EmailData
+    {
+        public string EmailToId { get; set; }
+        public string EmailToName { get; set; }
+        public string EmailSubject { get; set; }
+        public string EmailBody { get; set; }
+
+    }
+}
---
+++ NHASoftware/Services/EmailService.cs
@@ -0,0 +1,53 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using NHASoftware.Configuration;
+using NHASoftware.Models;
+
+namespace NHASoftware.Services
+{
+    public interface IEmailService
+    {
+        bool SendEmail(EmailData emailData);
+    }
+
+    public class EmailService : IEmailService
+    {
+        private EmailSettings _emailSettings = null;
+
+        public EmailService(IOptions<EmailSettings> options)
+        {
+            _emailSettings = options.Value;
+        }
+
+        public bool SendEmail(EmailData emailData)
+        {
+            try
+            {
+                MimeMessage emailMessage = new MimeMessage();
+                MailboxAddress emailFrom = new MailboxAddress(_emailSettings.Name, _emailSettings.EmailId);
+                emailMessage.From.Add(emailFrom);
+                MailboxAddress emailTo = new MailboxAddress(emailData.EmailToName, emailData.EmailToId);
+                emailMessage.To.Add(emailTo);
+                emailMessage.Subject = emailData.EmailSubject;
+                BodyBuilder emailBodyBuilder = new BodyBuilder();
+                emailBodyBuilder.TextBody = emailData.EmailBody;
+                emailMessage.Body = emailBodyBuilder.ToMessageBody();
+                SmtpClient emailClient = new SmtpClient();
+                emailClient.Connect(_emailSettings.Host, _emailSettings.Port, _emailSettings.UseSSL);
+                emailClient.Authenticate(_emailSettings.EmailId, _emailSettings.Password);
+                emailClient.Send(emailMessage);
+                emailClient.Disconnect(true);
+                emailClient.Dispose();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+
+}
