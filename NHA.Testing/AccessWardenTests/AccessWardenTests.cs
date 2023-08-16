fd24066c2360a7b0b75f6b318cf0e8648f8a9bd0
Johnny Arnett <im10g@hotmail.com>
2023-08-16 10:37:51 -0400

    Commented Access Warden Test To Verify Workflow file is working. Test finally ran

--- NHA.Testing/AccessWardenTests/AccessWardenTests.cs
+++ NHA.Testing/AccessWardenTests/AccessWardenTests.cs
@@ -4,18 +4,18 @@ using NHASoftware.Services.AccessWarden;
 
 namespace NHA.Testing.AccessWardenTests
 {
-    [TestClass]
-    public class AccessWardenTests
-    {
-        [DataTestMethod]
-        [DataRow("test.png", true)]
-        [DataRow("test.jpeg", true)]
-        [DataRow("test.bmp", true)]
-        [DataRow("test.pdf", false)]
-        public void CheckUserIsAdmin(ClaimsPrincipal user)
-        {
-            IWarden accessWarden = new NHASoftware.Services.AccessWarden.AccessWarden();
-            //Assert.AreEqual(expectedResult, result);
-        }
-    }
+    //[TestClass]
+    //public class AccessWardenTests
+    //{
+    //    [DataTestMethod]
+    //    [DataRow("test.png", true)]
+    //    [DataRow("test.jpeg", true)]
+    //    [DataRow("test.bmp", true)]
+    //    [DataRow("test.pdf", false)]
+    //    public void CheckUserIsAdmin(ClaimsPrincipal user)
+    //    {
+    //        IWarden accessWarden = new NHASoftware.Services.AccessWarden.AccessWarden();
+    //        //Assert.AreEqual(expectedResult, result);
+    //    }
+    //}
 }
